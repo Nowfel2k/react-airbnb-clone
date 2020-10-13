1233b5dffaec0d139c0e6ecb6470490924a4fd6f
Navfal <55524903+Nowfel2k@users.noreply.github.com>
2020-10-13 14:39:39 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB_OsfZM_HKBWKC_KZiVuxdm2Bx7E_rS4Q",
-  authDomain: "airbnb-clone-nowfel2k.firebaseapp.com",
-  databaseURL: "https://airbnb-clone-nowfel2k.firebaseio.com",
-  projectId: "airbnb-clone-nowfel2k",
-  storageBucket: "airbnb-clone-nowfel2k.appspot.com",
-  messagingSenderId: "386044522705",
-  appId: "1:386044522705:web:33ec61b242ad62fc1a15f0",
-  measurementId: "G-RQL5BFTQPS"
+  apiKey: "AIz",
+  authDomain: "airbnb-clonecom",
+  databaseURL: "https://airbnb-clon",
+  projectId: "airbnb-cl",
+  storageBucket: "airbnb-clone-com",
+  messagingSenderId: "",
+  appId: "1::web:",
+  measurementId: ""
 };
